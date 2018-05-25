20793ced3929da75c0e1b10ae0867d8da7a5668f
ankitsejwal <ankit@sejw.al>
2018-05-26 04:16:46 +1000

    javascript changeImage

--- javascript/code.js
+++ javascript/code.js
@@ -1,10 +1,4 @@
 function func(){
 
-    var paragraphs = document.getElementsByClassName('para');
-    
-    firstPara = paragraphs[0].innerHTML;
-    secondPara = paragraphs[1].innerHTML;
-
-    thirdPara = paragraphs[2].innerHTML = firstPara + secondPara 
-
+    document.getElementById('tajImg').src = "https://secure.i.telegraph.co.uk/multimedia/archive/02492/taj-pinl_2492833b.jpg";
 }
