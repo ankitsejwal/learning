66eb1ee55b0afaef3aa52d6991580de3a990ab2f
ankitsejwal <ankit@sejw.al>
2018-05-26 04:07:34 +1000

    javascript add innerHTML

--- javascript/code.js
+++ javascript/code.js
@@ -1,5 +1,10 @@
 function func(){
 
-    document.getElementById('para').innerHTML = 'wow i am changed now ...'
-  
+    var paragraphs = document.getElementsByClassName('para');
+    
+    firstPara = paragraphs[0].innerHTML;
+    secondPara = paragraphs[1].innerHTML;
+
+    thirdPara = paragraphs[2].innerHTML = firstPara + secondPara 
+
 }
