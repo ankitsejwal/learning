78f9a8d61d38b28b4a8eebecd7d0ff9770ec9af6
ankitsejwal <ankit@sejw.al>
2018-06-03 00:59:59 +1000

    update modules.js

--- node-mosh/modules.js
+++ node-mosh/modules.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const os = require('os');
+const fs = require('fs');
 
 const pathObj = path.parse(__filename)
 
@@ -12,3 +13,15 @@ var freeMemory = os.freemem;
 
 console.log('Total memory: ' + totalMemory);
 console.log(`Free memory: ${freeMemory}`);      // using javascript string literals
+
+// using fs module
+
+// synchronous method
+var files = fs.readdirSync('./');
+console.log(files);
+
+// asynchronous method
+var result = fs.readdir('./', function(err, files){
+    if(err) console.log('Error', err)
+    else console.log('Files: ', files)
+})
