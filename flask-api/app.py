5524567c847ccedcc300ef1f6622d7fb992bdd64
ankitsejwal <ankit@sejw.al>
2018-06-15 20:00:20 +1000

    implement endpoints

--- flask-api/app.py
+++ flask-api/app.py
@@ -27,7 +27,7 @@ stores = [
 def home():
     return render_template('index.html')
 
-
+# GET store with store numbers
 @app.route('/store/no/<int:number>')
 def store_number(number):
     store_length = len(stores)
@@ -40,8 +40,8 @@ def store_number(number):
 def create_store():
     request_data = request.get_json()
     new_store = {
-        'name': request_data['name'],
-        'item': []
+        "name": request_data["name"],
+        "item": []
     }
     stores.append(new_store)
     return jsonify(new_store)
@@ -63,22 +63,22 @@ def get_stores():
 @app.route('/store/<string:name>/item', methods=['POST'])
 def create_item_in_store(name):
     request_data = request.get_json()
+    new_item = {
+        "name": request_data["name"],
+        "price": request_data["price"]
+    }
     for store in stores:
-        if store['name'] == name:
-            new_item = {
-                'name': request_data['name'],
-                'price': request_data['item']
-            }
-            store['items'].append(new_item)
+        if store["name"] == name:
+            store["items"].append(new_item)
             return jsonify(new_item)
-    return jsonify({'error': 'store not found'})
+    return jsonify({"message": "Store not found!"})
+    
 
 @app.route('/store/<string:name>/item')
 def get_item_in_store(name):
     for store in stores:
-        if store['name'] == name:
-            return jsonify({'items': store['item']})
-        else:
-            return jsonify({'message': 'store not found'})
+        if store["name"] == name:
+            return jsonify({"items": store.items})
+    return jsonify({"message": "store not found"})
 
 app.run(port = 5000, debug=True)
