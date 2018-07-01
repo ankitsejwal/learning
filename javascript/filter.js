ee73f67b4c9da88df6bec4c7e769b49294037f7b
ankitsejwal <ankit@sejw.al>
2018-07-01 22:48:55 +1000

    chaining methods

--- javascript/filter.js
+++ javascript/filter.js
@@ -9,3 +9,10 @@ const items = positiveNumbers.map(n => `<li>${n}</li>`)
 // sandwitch it between ul tags
 const html = `<ul>${items.join('')}</ul>`
 console.log(html)
+
+// chaining methods
+const items2 = numbers
+    .filter(n => n >=0 )
+    .map(n => ({value: n}));
+
+console.log(items2)
