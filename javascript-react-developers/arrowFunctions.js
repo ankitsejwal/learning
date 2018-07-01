78be66f1e558d4c9f2b1bfabf5079cd5e15863ad
ankitsejwal <ankit@sejw.al>
2018-07-01 14:02:28 +1000

    arrow functions

---
+++ javascript-react-developers/arrowFunctions.js
@@ -0,0 +1,12 @@
+// Old way
+const square = function (number) {
+    return number * number;
+}
+
+// new way
+const square = (number) => {
+    return number * number;
+}
+
+// short hand 
+const square = number => number * number;
