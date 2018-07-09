41421f0c683aa312c81e6014fde47c979fd39b45
ankitsejwal <ankit@sejw.al>
2018-07-09 23:30:54 +1000

    hoisting

---
+++ object-oriented-javascript/hoisting.js
@@ -0,0 +1,13 @@
+// function declaration
+function sayHello(){}
+
+// function expression
+const sayGoodBye = function(){};
+
+// class declaration
+class Cat{
+
+}
+
+// class expression
+const Dog = class{};
