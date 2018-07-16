f323691979f1fc9ca19eb59824072f1ac7f9487f
ankitsejwal <ankit@sejw.al>
2018-07-16 23:57:33 +1000

    update

--- javascript-algorithm/main.js
+++
@@ -1,9 +0,0 @@
-// reverse a string
-
-let string = 'apple';
-let reversedString = '';
-
-for (let x = string.length - 1; x >= 0; x--)
-    reversedString += string[x];
-
-console.log(reversedString);
