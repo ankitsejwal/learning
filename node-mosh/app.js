46527f796ab6e7bb2a71733533a689f2d4b807c8
ankitsejwal <ankit@sejw.al>
2018-08-14 00:26:13 +1000

    handling http get request

--- node-mosh/app.js
+++ node-mosh/app.js
@@ -1,17 +1,25 @@
 const express = require('express');
 const app = express();
 
+const courses = [
+    { id: 1, name: 'course1' },
+    { id: 2, name: 'course2' },
+    { id: 3, name: 'course3' }
+]
+
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
 app.get('/api/courses', (req, res) => {
-    res.send([1, 2, 3]);
+    res.send(courses);
 });
 
 // dealing with parameters like /api/courses/1
 app.get('/api/course/:id', (req, res) => {
-    res.send(req.params.id);
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) res.status(404).send('Course not found!!!');
+    res.send(course);
 });
 
 const port = process.env.PORT || 3000;
